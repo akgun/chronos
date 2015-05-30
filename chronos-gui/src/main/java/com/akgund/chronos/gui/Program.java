744e3180e8864dc23b2bb2cc423c681c9fccb613
Akgün Demirbaş <akgun.demirbas@gmail.com>
2015-05-30 20:28:03 +0300

    Added nimbus interface.

--- chronos-gui/src/main/java/com/akgund/chronos/gui/Program.java
+++ chronos-gui/src/main/java/com/akgund/chronos/gui/Program.java
@@ -1,10 +1,22 @@
 package com.akgund.chronos.gui;
 
 import javax.swing.*;
+import java.util.stream.Stream;
 
 public class Program {
 
     public static void main(String[] args) {
+        try {
+            UIManager.LookAndFeelInfo lookAndFeelInfo = Stream.of(UIManager.getInstalledLookAndFeels()).filter(info ->
+                    "Nimbus".equals(info.getName())).findFirst().get();
+            UIManager.setLookAndFeel(lookAndFeelInfo.getClassName());
+        } catch (Exception e) {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e1) {
+            }
+        }
+
         SwingUtilities.invokeLater(() -> {
             ChronosGUI chronosGUI = new ChronosGUI();
             chronosGUI.setVisible(true);
