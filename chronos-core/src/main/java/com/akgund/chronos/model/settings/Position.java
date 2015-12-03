4d6f86e13a9100b9531984cec3c1338923e72f0f
akgun demirbas <akgun.demirbas@gmail.com>
2015-12-03 19:06:55 +0200

    Added position setting check.

--- chronos-core/src/main/java/com/akgund/chronos/model/settings/Position.java
+++ chronos-core/src/main/java/com/akgund/chronos/model/settings/Position.java
@@ -5,18 +5,34 @@ public class Position {
     private int y;
 
     public int getX() {
+        if (x < 0) {
+            return 0;
+        }
+        
         return x;
     }
 
     public void setX(int x) {
+        if (x < 0) {
+            x = 0;
+        }
+
         this.x = x;
     }
 
     public int getY() {
+        if (y < 0) {
+            return 0;
+        }
+
         return y;
     }
 
     public void setY(int y) {
+        if (y < 0) {
+            y = 0;
+        }
+
         this.y = y;
     }
 }
