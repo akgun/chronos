acede31f177824f0fd1b4e65782748d929bc1cb2
Akgun Demirbas <akgun.demirbas@gmail.com>
2016-06-05 08:52:00 +0300

    Added locale.

--- chronos-core/src/main/java/com/akgund/chronos/core/impl/DateTimeParser.java
+++ chronos-core/src/main/java/com/akgund/chronos/core/impl/DateTimeParser.java
@@ -3,13 +3,16 @@ package com.akgund.chronos.core.impl;
 import org.joda.time.DateTime;
 import org.joda.time.format.ISODateTimeFormat;
 
+import java.util.Locale;
+
 public class DateTimeParser {
+    private Locale locale = Locale.forLanguageTag("tr");
 
     public DateTime parse(String dateTimeStr) {
-        return ISODateTimeFormat.dateTime().parseDateTime(dateTimeStr);
+        return ISODateTimeFormat.dateTime().withLocale(locale).parseDateTime(dateTimeStr);
     }
 
     public String toString(DateTime dateTime) {
-        return ISODateTimeFormat.dateTime().print(dateTime);
+        return ISODateTimeFormat.dateTime().withLocale(locale).print(dateTime);
     }
 }
